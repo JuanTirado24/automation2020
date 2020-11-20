3921c2e39637b5a0f4be9e90b8cf893ce9529982
JUAN <JUAN@Juan>
2020-11-20 00:11:41 -0500

    first commit

---
+++ Selenium0711/src/main/java/seleniumEj/SelTest.java
@@ -0,0 +1,24 @@
+package seleniumEj;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+		System.out.println("SelTest.main()" + "Adicion");
+		// el file.separator indica el / y la idea es traer la ruta del proyecto.
+		String exePath = Paths.get("").toAbsolutePath().toString()+File.separator + "drivers"+File.separator;
+		
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); // identificando el exe para driver
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+
+}
---
+++ Selenium0711/src/test/java/Test/TestNgEjemplo.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNgEjemplo {
+  @Test
+  public void f() {
+	  
+	  Assert.assertTrue(true);
+  }
+}
